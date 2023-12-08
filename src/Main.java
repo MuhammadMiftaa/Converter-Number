49397dbc693c1082886905c174f5d1f3dbea8d9c
Muhammad Miftakul Salam <140779680+MuhammadMiftaa@users.noreply.github.com>
2023-12-08 09:05:17 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,87 +8,146 @@ public class Main {
         System.out.print("1. Decimal\n2. Binary\n3. Octal\n4. Hexadecimal  >>>  ");
         int intialChoice = scanner.nextInt();
         String typeChoice;
+        String status = "";
         DecimalHexadecimalClass convert = new DecimalHexadecimalClass(intialChoice);
 
-        if(intialChoice == 1){typeChoice = "'Decimal'";} else if(intialChoice == 2){typeChoice = "'Binary'";}else if(intialChoice == 3){typeChoice = "'Octal'";}else if(intialChoice == 4){typeChoice = "'Hexadecimal'";}else{typeChoice = "'invalid'";}
-        System.out.println("So, you chose "+typeChoice+".");
-        System.out.println("\nWhat type of number would you like to convert it to?");
-        System.out.print("1. Decimal\n2. Binary\n3. Octal\n4. Hexadecimal\n5. Show All\t\t>>>  ");
-        int goalChoice = scanner.nextInt();
+        if(intialChoice == 1){typeChoice = "'Decimal'";} else if(intialChoice == 2){typeChoice = "'Binary'";}else if(intialChoice == 3){typeChoice = "'Octal'";}else if(intialChoice == 4){typeChoice = "'Hexadecimal'";}else{typeChoice = " ";}
+        int goalChoice = 0;
+        if(typeChoice != " "){
+            System.out.println("So, you chose "+typeChoice+".");
+            System.out.println("\nWhat type of number would you like to convert it to?");
+            System.out.print("1. Decimal\n2. Binary\n3. Octal\n4. Hexadecimal\t\t>>>  ");
+            goalChoice = scanner.nextInt();
+        }
 
-        if(intialChoice == 1){
-            System.out.print("What number you want to change? (decimal) >>> ");
+        if(intialChoice == 1 && (goalChoice >= 1 && goalChoice <= 4)){
+            System.out.print("What number do you want to change? (decimal) >>> ");
             convert.intNumber = scanner.nextInt();
-            if (goalChoice == 1){
-                System.out.print("\nSuccesfull.\nHere's the result (decimal) >>> "+convert.toDecimal(convert.intNumber));
-            } else if(goalChoice == 2){
-                System.out.print("\nSuccesfull.\nHere's the result (binary) >>> "+convert.toBinary(convert.intNumber));
-            } else if(goalChoice == 3){
-                System.out.print("\nSuccesfull.\nHere's the result (octal) >>> "+convert.toOctal(convert.intNumber));
-            } else if(goalChoice == 4){
-                System.out.print("\nSuccesfull.\nHere's the result (hexadecimal) >>> "+convert.toHexadecimal(convert.intNumber));
-            } else if(goalChoice == 5){
-                System.out.print("\nSuccesfull.\nYour number (decimal) >>> "+convert.toDecimal(convert.intNumber));
-                System.out.print("\nHere's the result\n(binary)\t\t>>> "+convert.toBinary(convert.intNumber));
-                System.out.print("\n(octal)\t\t\t>>> "+convert.toOctal(convert.intNumber));
-                System.out.print("\n(hexadecimal)\t>>> "+convert.toHexadecimal(convert.intNumber));
+
+            String result = "";
+            if (goalChoice == 1) {
+                result = convert.toDecimal(convert.intNumber);
+            } else if (goalChoice == 2) {
+                result = convert.toBinary(convert.intNumber);
+            } else if (goalChoice == 3) {
+                result = convert.toOctal(convert.intNumber);
+            } else if (goalChoice == 4) {
+                result = convert.toHexadecimal(convert.intNumber);
             }
-        } else if (intialChoice == 2) {
-            System.out.print("What number you want to change? (binary) >>> ");
+
+            if ("Input Invalid.".equals(result)) {
+                // Jika input tidak valid
+                System.out.print("\nFailed.\n" + result);
+                status = "Failed";
+            } else {
+                // Jika input valid
+                System.out.print("\nSuccessful.\nHere's the result >>> " + result);
+                status = "Success";
+            }
+//           else if(goalChoice == 5){
+//                System.out.print("\nSuccesfull.\nYour number (decimal) >>> "+convert.toDecimal(convert.intNumber));
+//                System.out.print("\nHere's the result\n(binary)\t\t>>> "+convert.toBinary(convert.intNumber));
+//                System.out.print("\n(octal)\t\t\t>>> "+convert.toOctal(convert.intNumber));
+//                System.out.print("\n(hexadecimal)\t>>> "+convert.toHexadecimal(convert.intNumber));
+//            }
+        } else if (intialChoice == 2 && (goalChoice >= 1 && goalChoice <= 4)) {
+            System.out.print("What number do you want to change? (binary) >>> ");
             convert = new BinaryClass(intialChoice);
             convert.intNumber = scanner.nextInt();
-            if (goalChoice == 1){
-                System.out.print("\nSuccesfull.\nHere's the result (decimal) >>> "+convert.toDecimal(convert.intNumber));
-            } else if(goalChoice == 2){
-                System.out.print("\nSuccesfull.\nHere's the result (binary) >>> "+convert.toBinary(convert.intNumber));
-            } else if(goalChoice == 3){
-                System.out.print("\nSuccesfull.\nHere's the result (octal) >>> "+convert.toOctal(convert.intNumber));
-            } else if(goalChoice == 4){
-                System.out.print("\nSuccesfull.\nHere's the result (hexadecimal) >>> "+convert.toHexadecimal(convert.intNumber));
-            } else if(goalChoice == 5){
-                System.out.print("\nSuccesfull.\nYour number (binary) >>> "+convert.toBinary(convert.intNumber));
-                System.out.print("\nHere's the result\n(decimal)\t\t>>> "+convert.toDecimal(convert.intNumber));
-                System.out.print("\n(octal)\t\t\t>>> "+convert.toOctal(convert.intNumber));
-                System.out.print("\n(hexadecimal)\t>>> "+convert.toHexadecimal(convert.intNumber));
+
+            String result = "";
+            if (goalChoice == 1) {
+                result = convert.toDecimal(convert.intNumber);
+            } else if (goalChoice == 2) {
+                result = convert.toBinary(convert.intNumber);
+            } else if (goalChoice == 3) {
+                result = convert.toOctal(convert.intNumber);
+            } else if (goalChoice == 4) {
+                result = convert.toHexadecimal(convert.intNumber);
             }
-        } else if (intialChoice == 3) {
-            System.out.print("What number you want to change? (octal) >>> ");
+
+            if ("Input Invalid.".equals(result)) {
+                // Jika input tidak valid
+                System.out.print("\nFailed.\n" + result);
+                status = "Failed";
+            } else {
+                // Jika input valid
+                System.out.print("\nSuccessful.\nHere's the result >>> " + result);
+                status = "Success";
+            }
+//            else if(goalChoice == 5){
+//                System.out.print("\nSuccesfull.\nYour number (binary) >>> "+convert.toBinary(convert.intNumber));
+//                System.out.print("\nHere's the result\n(decimal)\t\t>>> "+convert.toDecimal(convert.intNumber));
+//                System.out.print("\n(octal)\t\t\t>>> "+convert.toOctal(convert.intNumber));
+//                System.out.print("\n(hexadecimal)\t>>> "+convert.toHexadecimal(convert.intNumber));
+//            }
+        } else if (intialChoice == 3 && (goalChoice >= 1 && goalChoice <= 4)) {
+            System.out.print("What number do you want to change? (octal) >>> ");
             convert = new OctalClass(intialChoice);
             convert.intNumber = scanner.nextInt();
-            if (goalChoice == 1){
-                System.out.print("\nSuccesfull.\nHere's the result (decimal) >>> "+convert.toDecimal(convert.intNumber));
-            } else if(goalChoice == 2){
-                System.out.print("\nSuccesfull.\nHere's the result (binary) >>> "+convert.toBinary(convert.intNumber));
-            } else if(goalChoice == 3){
-                System.out.print("\nSuccesfull.\nHere's the result (octal) >>> "+convert.toOctal(convert.intNumber));
-            } else if(goalChoice == 4){
-                System.out.print("\nSuccesfull.\nHere's the result (hexadecimal) >>> "+convert.toHexadecimal(convert.intNumber));
-            } else if(goalChoice == 5){
-                System.out.print("\nSuccesfull.\nYour number (octal) >>> "+convert.toOctal(convert.intNumber));
-                System.out.print("\nHere's the result\n(decimal)\t\t>>> "+convert.toDecimal(convert.intNumber));
-                System.out.print("\n(binary)\t\t>>> "+convert.toBinary(convert.intNumber));
-                System.out.print("\n(hexadecimal)\t>>> "+convert.toHexadecimal(convert.intNumber));
+
+            String result = "";
+            if (goalChoice == 1) {
+                result = convert.toDecimal(convert.intNumber);
+            } else if (goalChoice == 2) {
+                result = convert.toBinary(convert.intNumber);
+            } else if (goalChoice == 3) {
+                result = convert.toOctal(convert.intNumber);
+            } else if (goalChoice == 4) {
+                result = convert.toHexadecimal(convert.intNumber);
             }
-        } else if (intialChoice == 4){
-            System.out.print("What number you want to change? (hexadecimal) >>> ");
+
+            if ("Input Invalid.".equals(result)) {
+                // Jika input tidak valid
+                System.out.print("\nFailed.\n" + result);
+                status = "Failed";
+            } else {
+                // Jika input valid
+                System.out.print("\nSuccessful.\nHere's the result >>> " + result);
+                status = "Success";
+            }
+//            else if(goalChoice == 5){
+//                System.out.print("\nSuccesfull.\nYour number (octal) >>> "+convert.toOctal(convert.intNumber));
+//                System.out.print("\nHere's the result\n(decimal)\t\t>>> "+convert.toDecimal(convert.intNumber));
+//                System.out.print("\n(binary)\t\t>>> "+convert.toBinary(convert.intNumber));
+//                System.out.print("\n(hexadecimal)\t>>> "+convert.toHexadecimal(convert.intNumber));
+//            }
+        } else if (intialChoice == 4 && (goalChoice >= 1 && goalChoice <= 4)){
+            System.out.print("What number do you want to change? (hexadecimal) >>> ");
             convert = new DecimalHexadecimalClass(intialChoice);
             convert.strNumber = scanner.next();
-            if (goalChoice == 1){
-                System.out.print("\nSuccesfull.\nHere's the result (decimal) >>> "+convert.toDecimal(convert.strNumber));
-            } else if(goalChoice == 2){
-                System.out.print("\nSuccesfull.\nHere's the result (binary) >>> "+convert.toBinary(convert.strNumber));
-            } else if(goalChoice == 3){
-                System.out.print("\nSuccesfull.\nHere's the result (octal) >>> "+convert.toOctal(convert.strNumber));
-            } else if(goalChoice == 4){
-                System.out.print("\nSuccesfull.\nHere's the result (hexadecimal) >>> "+convert.toHexadecimal(convert.strNumber));
-            } else if(goalChoice == 5){
-                System.out.print("\nSuccesfull.\nYour number (hexadecimal) >>> "+convert.toHexadecimal(convert.strNumber));
-                System.out.print("\nHere's the result\n(decimal)\t\t>>> "+convert.toDecimal(convert.strNumber));
-                System.out.print("\n(binary)\t\t>>> "+convert.toBinary(convert.strNumber));
-                System.out.print("\n(octal)\t\t\t>>> "+convert.toOctal(convert.strNumber));
+
+            String result = "";
+            if (goalChoice == 1) {
+                result = convert.toDecimal(convert.strNumber);
+            } else if (goalChoice == 2) {
+                result = convert.toBinary(convert.strNumber);
+            } else if (goalChoice == 3) {
+                result = convert.toOctal(convert.strNumber);
+            } else if (goalChoice == 4) {
+                result = convert.toHexadecimal(convert.strNumber);
             }
-        } else {
-            System.out.println("\nInvalid Number.");
+
+            if ("Input Invalid.".equals(result)) {
+                // Jika input tidak valid
+                System.out.print("\nFailed.\n" + result);
+                status = "Failed";
+            } else {
+                // Jika input valid
+                System.out.print("\nSuccessful.\nHere's the result >>> " + result);
+                status = "Success";
+            }
+//            else if(goalChoice == 5){
+//                System.out.print("\nSuccesfull.\nYour number (hexadecimal) >>> "+convert.toHexadecimal(convert.strNumber));
+//                System.out.print("\nHere's the result\n(decimal)\t\t>>> "+convert.toDecimal(convert.strNumber));
+//                System.out.print("\n(binary)\t\t>>> "+convert.toBinary(convert.strNumber));
+//                System.out.print("\n(octal)\t\t\t>>> "+convert.toOctal(convert.strNumber));
+//            }
         }
+        else {
+            System.out.println("\nInvalid Choice Type.");
+        }
+
+        System.out.println(status);
     }
 }
